2d0ce3a23166478f2d975a31fbbe67c218458b0c
ahnsungchul <81339776+ahnsungchul@users.noreply.github.com>
2024-05-07 16:49:18 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -19,3 +19,56 @@ $(document).ready(function(){
 	});
 
 });
+
+const myService = {
+    lp : function(){
+        const _lpID = "lpMyService";
+        const _val = $("#myService").val();
+        if(_val != 0){
+            const _option = $("#myService option:selected").text();
+            $("#lp_layout").addClass("on");
+            $("#lp_layout .lp_wrap."+_lpID).addClass("on");
+            $("html").css("overflow","hidden");
+            $("#lp_layout .lp_wrap."+_lpID+" .service p").text(_option);
+        }else{
+            $(document).find(".errorShow[data-txtError=myService]").show();
+        }
+        myService.select();
+    },
+    select : function(){
+        const _val = $("#myService").val();
+        if(_val != 0){
+            $(document).find(".errorShow[data-txtError=myService]").hide();
+            $("#myService").css({"color":"#161616"});
+        }else{
+            $("#myService").removeAttr("style");
+        }
+    }
+}
+
+const serviceMake = {
+    lp : function(){
+        const _val = $("#datasetMake").val();
+        if(_val != 0){
+        }else{
+            $(document).find(".errorShow[data-txtError=datasetMake]").show();
+        }
+        myService.select();
+    },
+    select : function(){
+        const _val = $("#datasetMake").val();
+        if(_val != 0){
+            $(document).find(".errorShow[data-txtError=datasetMake]").hide();
+            $("#myService").css({"color":"#161616"});
+        }else{
+            $("#myService").removeAttr("style");
+        }
+    }
+}
+
+const datasetSelect = function(select){
+    const _this = $(select);
+    const _val = _this.val();
+    $(document).find(".list-card").hide();
+    $(document).find(".list-card[data-dataset="+_val+"]").show();
+}
